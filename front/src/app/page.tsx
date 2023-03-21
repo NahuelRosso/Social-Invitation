09d3de0194330a6b55744124f4cbfca1603bc258
CanarioTOMAS <tomimartine.34@gmail.co>
2023-03-21 11:12:25 -0300

    work in progress

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -3,13 +3,14 @@ import styles from './page.module.css'
 
 import IFooter, { BottomItem } from "@/shared/components/footer/footer";
 import { Amcecis } from "pages/amcecis/amcecis";
+import ContactForm from '@/shared/components/form_contact/form-contact';
 
 
 export default function Home() {         
   return (
     <main>
       <div >
-      <Amcecis/>
+      <ContactForm/>
        
       </div>
     </main>
--- front/src/shared/components/form_contact/form-contact.tsx
+++ front/src/shared/components/form_contact/form-contact.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import {
   Box,
   Button,
@@ -11,82 +10,105 @@ import {
   Typography,
 } from "@mui/material";
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+
+interface FormData {
+  firstName: string;
+  lastName: string;
+  email: string;
+  address: string;
+  phone: string;
+  message: string;
+}
 
 function ContactForm() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-  const [surname, setSurname] = useState("");
-  const [phone, setPhone] = useState("");
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    formState: { errors },
+  } = useForm<FormData>();
 
-  const handleSubmit = (event: { preventDefault: () => void }) => {
-    event.preventDefault();
-    console.log("Submitting form", name, email, message, surname);
-    // Aquí podrías enviar la información del formulario al servidor utilizando fetch o axios, por ejemplo.
-  };
+  const onSubmit = handleSubmit((values) => {
+    alert(JSON.stringify(values));
+  });
 
   return (
-    <Container sx={{ mt: 9, mb:10 }} maxWidth="sm">
+    <Container sx={{ mt: 9, mb: 10 }} maxWidth="sm">
       <Grid
         container
         direction="column"
         alignItems="center"
         justifyContent="center"
-        sx={{ ninHeight: "100vh" }}
+        sx={{ minHeight: "100vh" }}
       >
         <Grid item>
           <Paper sx={{ padding: "1.2em", borderRadius: "0.5em" }}>
             <Typography sx={{ mt: 1, mb: 1 }} variant="h4" align="center">
               Contáctanos
             </Typography>
-            <Box component="form" onSubmit={handleSubmit}>
+            <Box component="form" onSubmit={onSubmit}>
               <TextField
-                name="name"
+                {...(errors.firstName && { helperText: "Campo Obligatorio." })}
+                error={!!errors.firstName}
                 fullWidth
                 type="text"
                 label="Nombre"
                 autoComplete="off"
                 sx={{ mt: 2, mb: 1.5 }}
-                onChange={(event) => setName(event.target.value)}
+                {...register("firstName", { required: true, minLength: 3 })}
               />
               <TextField
-                name="surname"
+                {...(errors.lastName && { helperText: "Campo Obligatorio." })}
+                {...register("lastName", { required: true })}
+                error={!!errors.lastName}
                 fullWidth
                 type="text"
                 label="Apellido"
                 autoComplete="off"
                 sx={{ mt: 1.5, mb: 1.5 }}
-                onChange={(event) => setSurname(event.target.value)}
               />
               <TextField
-                name="email"
+                {...(errors.email && { helperText: "Campo Obligatorio." })}
+                {...register("email", { required: true,  pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/i  })}
+                error={!!errors.email}
                 fullWidth
-                type="email"
                 label="Email"
                 autoComplete="off"
                 sx={{ mt: 1.5, mb: 1.5 }}
-                onChange={(event) => setEmail(event.target.value)}
               />
               <TextField
-                name="phone"
+                {...(errors.phone && { helperText: "Campo Obligatorio." })}
+                {...register("phone", { required: true })}
+                error={!!errors.phone}
                 fullWidth
                 type="text"
                 label="Teléfono"
                 autoComplete="off"
                 sx={{ mt: 1.5, mb: 1.5 }}
-                onChange={(event) => setPhone(event.target.value)}
               />
-              
               <TextField
-                name="message"
+                {...(errors.address && { helperText: "Campo Obligatorio." })}
+                error={!!errors.address}
+                {...register("address", { required: true })}
                 fullWidth
                 type="text"
+                label="Dirección"
+                autoComplete="off"
+                sx={{ mt: 1.5, mb: 1.5 }}
+              />
+
+              <TextField
+                {...(errors.message && { helperText: "Campo Obligatorio." })}
+                {...register("message", { required: true })}
+                fullWidth
+                error={!!errors.message}
+                type="textarea"
                 label="Deje su mensaje aqui"
                 multiline
                 autoComplete="off"
-                maxRows={4}
+                rows={4}
                 sx={{ mt: 1.5, mb: 1.5 }}
-                onChange={(event) => setMessage(event.target.value)}
               />
               <Button
                 fullWidth
